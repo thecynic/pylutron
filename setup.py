471f9129d09230386c5f0d8f45f6543d7635d803
Dima Zavin <thecynic@gmail.com>
2018-06-03 15:32:05 -0700

    Update version to 0.2.0 for PyPi

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'pylutron',
-    version = '0.1.1',
+    version = '0.2.0',
     license = 'MIT',
     description = 'Python library for Lutron RadioRA 2',
     author = 'Dima Zavin',
