5bbe736a516125605f0e2e193bf5f52596824b52
Andrew Chatham <andrew@andrewchatham.com>
2018-12-21 13:58:20 -0800

    Add docstring

--- pylutron/__init__.py
+++ pylutron/__init__.py
@@ -967,6 +967,7 @@ class MotionSensor(LutronEntity):
 
 
 class OccupancyGroup(LutronEntity):
+  """Represents one or more occupancy/vacancy sensors grouped into an Area."""
   _CMD_TYPE = 'GROUP'
   _ACTION_STATE = 3
 
