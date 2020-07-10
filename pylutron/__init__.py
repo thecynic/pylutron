2c29d9b722b06835d6fad3384a40ba5f720f5997
cdheiser <10488026+cdheiser@users.noreply.github.com>
2020-07-09 20:26:32 -0700

    Add support for raising/lowering/stopping shades. (#57)
    * Add support for raising/lowering/stopping shades.
    
    This will allow the Home Assistant integration to resolve an outstanding
    feature request.
    
    Co-authored-by: cdheiser <cdheiser@users.noreply.github.com>

--- pylutron/__init__.py
+++ pylutron/__init__.py
@@ -308,13 +308,17 @@ class LutronXmlDbParser(object):
   def _parse_output(self, output_xml):
     """Parses an output, which is generally a switch controlling a set of
     lights/outlets, etc."""
-    output = Output(self._lutron,
-                    name=output_xml.get('Name'),
-                    watts=int(output_xml.get('Wattage')),
-                    output_type=output_xml.get('OutputType'),
-                    integration_id=int(output_xml.get('IntegrationID')),
-                    uuid=output_xml.get('UUID'))
-    return output
+    output_type = output_xml.get('OutputType')
+    kwargs = {
+      'name': output_xml.get('Name'),
+      'watts': int(output_xml.get('Wattage')),
+      'output_type': output_type,
+      'integration_id': int(output_xml.get('IntegrationID')),
+      'uuid': output_xml.get('UUID')
+    }
+    if output_type == 'SYSTEM_SHADE':
+      return Shade(self._lutron, **kwargs)
+    return Output(self._lutron, **kwargs)
 
   def _parse_keypad(self, keypad_xml, device_group):
     """Parses a keypad device (the Visor receiver is technically a keypad too)."""
@@ -746,6 +750,28 @@ class Output(LutronEntity):
   def is_dimmable(self):
     """Returns a boolean of whether or not the output is dimmable."""
     return self.type != 'NON_DIM' and not self.type.startswith('CCO_')
+
+
+class Shade(Output):
+  """This is the output entity for shades in Lutron universe."""
+  _ACTION_RAISE = 2
+  _ACTION_LOWER = 3
+  _ACTION_STOP = 4
+
+  def start_raise(self):
+    """Starts raising the shade."""
+    self._lutron.send(Lutron.OP_EXECUTE, Output._CMD_TYPE, self._integration_id,
+        Output._ACTION_RAISE)
+
+  def start_lower(self):
+    """Starts lowering the shade."""
+    self._lutron.send(Lutron.OP_EXECUTE, Output._CMD_TYPE, self._integration_id,
+        Output._ACTION_LOWER)
+
+  def stop(self):
+    """Starts raising the shade."""
+    self._lutron.send(Lutron.OP_EXECUTE, Output._CMD_TYPE, self._integration_id,
+        Output._ACTION_STOP)
 
 
 class KeypadComponent(LutronEntity):
