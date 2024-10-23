08cf3000f9c94b449099c5a13af281f0f5bd52f3
txwindsurfer <reagan.ramsower@gmail.com>
2024-10-22 20:20:19 -0500

    Add HWI_SLIM keypad (#108)
    Adding Homeworks Slim Keypads

--- pylutron/__init__.py
+++ pylutron/__init__.py
@@ -299,6 +299,7 @@ class LutronXmlDbParser(object):
             'MAIN_REPEATER',
             'HOMEOWNER_KEYPAD',
             'PALLADIOM_KEYPAD',
+            'HWI_SLIM’,
             'GRAFIK_T_HYBRID_KEYPAD'):
           keypad = self._parse_keypad(device_xml, device_group)
           area.add_keypad(keypad)
